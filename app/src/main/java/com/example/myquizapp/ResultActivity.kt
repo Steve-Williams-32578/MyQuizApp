119990edf701a8659bd704718cad10659d1367bc
Steve-Williams-32578 <70238486+Steve-Williams-32578@users.noreply.github.com>
2022-02-08 16:33:54 -0600

    I'm attempting to complete the results page on my own, without following the class guide.  It isn't finished yet.
    TODO: make results page and set it up for multiple plays

--- app/src/main/java/com/example/myquizapp/ResultActivity.kt
+++ app/src/main/java/com/example/myquizapp/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.myquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.android.synthetic.main.activ
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
